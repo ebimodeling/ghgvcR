4f3b08ac2742340182f85a978ff678ac8aba35d4
David LeBauer <dlebauer@illinois.edu>
2013-12-05 17:08:10 -0800

    revising ghgvc_script.R, inputs, and outputs to debug

--- src/ghgvc_script.R
+++ src/ghgvc_script.R
@@ -1,11 +1,14 @@
 #!/usr/bin/Rscript
 library(ghgvcr)
-config.xml <- system.file("multisite_config.xml", package = "ghgvcr")
 
+## TODO pass inputdir and outputdir as arguments
+inputdir <- "/home/ubuntu/ghgvcR/inst"
+outputdir <- "/home/ubuntu/ghgvcR/inst/extdata"
 
+config.xml <- file.path(inputdir, "multisite_config.xml")
 config.list <- xmlToList(xmlParse(config.xml))
 
 x <- ghgvc2(config.list)
 
-writeLines(x, "inst/extdata/output.json")
-write.csv(as.data.frame(fromJSON(x)), "inst/extdata/output.csv")
+writeLines(x, file.path(outputdir, "output.json"))
+write.csv(as.data.frame(fromJSON(x)), file.path(outputdir, "output.csv"))
