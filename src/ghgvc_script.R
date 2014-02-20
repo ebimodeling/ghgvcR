7a69f2137f2a23cf3f003ba5fdeb322e4259d766
David LeBauer <dlebauer@gmail.com>
2014-02-19 23:42:24 -0600

    updated colors

--- src/ghgvc_script.R
+++ src/ghgvc_script.R
@@ -55,7 +55,7 @@ CRV <- outdf$crv
 
 library(ggplot2)
 library(gridExtra)
-
+library(scales)
 library(Hmisc)
 
 plotdata <- data.frame(Biome = capitalize(paste(gsub("_", " ", gsub("BR", "Brazil", outdf$Biome)), "Site", outdf$Location)),
@@ -87,26 +87,25 @@ neg <- longdata$value <= 0
 bgc.plot <- baseplot +
   geom_bar(data = subset(longdata[pos,], variable %in% c("Storage", "Ongoing_Exchange")), 
            aes(x = Biome, y = value, fill = variable),  
-           width = 0.25, stat = "identity", color = "darkgrey") +  
+           width = 0.25, stat = "identity") +  
   geom_bar(data = subset(longdata[neg, ], variable %in% c("Storage", "Ongoing_Exchange")), 
            aes(x = Biome, y = value, fill = variable),  
-           width = 0.25, stat = "identity", color = "darkgrey") +  
-  scale_fill_manual(values= c("DarkGreen", "LightGreen"), labels = c("Storage", "Ongoing Exchange")) + labs(fill = "") +
+           width = 0.25, stat = "identity") +  
+  scale_fill_manual(values= brewer_pal(pal = "Greens")(6)[c(4,6)], labels = c("Storage", "Ongoing Exchange")) + labs(fill = "") +
   ggtitle("Biogeochemical") + theme(axis.text.y = element_text(size = 12, hjust = 1))
 biophys.plot <- baseplot +
   geom_bar(data = subset(longdata[pos, ], variable %in% c("Rnet", "LE")), aes(x = Biome, y = value, fill = variable),  
-           width = 0.25, stat = "identity", color = "darkgrey") +  
+           width = 0.25, stat = "identity") +  
   geom_bar(data = subset(longdata[neg, ], variable %in% c("Rnet", "LE")), aes(x = Biome, y = value, fill = variable),  
-           width = 0.25, stat = "identity", color = "darkgrey") +  
-  scale_fill_manual(values= c("LightBlue", "DarkBlue"), labels = c(expression("LE", "R"["net"]))) + labs(fill = "") + 
+           width = 0.25, stat = "identity") +  
+  scale_fill_manual(values = brewer_pal(pal = "Blues")(6)[c(4,6)], labels = c(expression("LE", "R"["net"]))) + labs(fill = "") + 
   ggtitle("Biophysical")
 crv.plot <- baseplot +
   geom_bar(data = subset(longdata[pos, ], variable %in% c("CRV_BGC", "CRV_BIOPHYS")), aes(x = Biome, y = value, fill = variable),  
-           width = 0.25, stat = "identity", color = "darkgrey") +  
+           width = 0.25, stat = "identity") +  
   geom_bar(data = subset(longdata[neg, ], variable %in% c("CRV_BGC", "CRV_BIOPHYS")), aes(x = Biome, y = value, fill = variable),  
-           width = 0.25, stat = "identity", color = "darkgrey") +  
-  
-  scale_fill_manual(values= c("LightGreen", "LightBlue"), labels = c("Biogeochemical", "Biophysical")) + labs(fill = "") +
+           width = 0.25, stat = "identity") +  
+  scale_fill_manual(values= c(brewer_pal(pal = "Greens")(6)[5], brewer_pal(pal = "Blues")(6)[5]), labels = c("Biogeochemical", "Biophysical")) + labs(fill = "") +
   geom_point(data = subset(longdata, variable == "CRV_NET"), aes(x = Biome, y = value)) +
   ggtitle("Climate Regulating Value")
 
