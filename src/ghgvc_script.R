0b53d6c8aa62d6b32c2f49f4e9c3a818b92ac2fa
David LeBauer <dlebauer@gmail.com>
2014-02-19 18:14:03 -0600

    started working on plots (gh-7)

--- src/ghgvc_script.R
+++ src/ghgvc_script.R
@@ -40,47 +40,54 @@ for(site in names(outlist)){
 
 colnames(outdf)[colnames(outdf) == "name"] <- "Biome"
 colnames(outdf) <- gsub("D_", "GHGV_", colnames(outdf))
+outdf$GHGV <- outdf$GHGV_CO2 + outdf$GHGV_CH4 + outdf$GHGV_N2O
 
 outdf <- outdf[order(outdf$Location),]
 write.csv(outdf, file.path(outdir, "output.csv"), row.names = FALSE)
 
 ## Plotting
-# initial_storage <- outdf[,grepl("S_", colnames(outdf))]
-# ongoing_exchange <- outdf[,grepl("F_", colnames(outdf))]
-# Total_GHGV <- outdf[,grepl("GHGV_", colnames(outdf))]
-# Biophysical <- data.frame(Rnet = outdf$swRFV, LE = outdf$latent)
-# CRV <- outdf$crv
-# 
-# ## T_E is number of years considered
-# T_E <- config.list$options$T_E
-# xlabels <- paste0("CO_2 Emission Equivalents (Mg CO_2-eq ha-1 ",  T_E, " yrs-1)")
-# 
-# library(ggplot2)
-# library(gridExtra)
-# 
-# library(Hmisc)
-# 
-# plotdata <- data.frame(Biome = capitalize(paste(gsub("_", " ", gsub("BR", "Brazil", outdf$Biome)), "Site", outdf$Location)),
-#                        Storage = rowSums(initial_storage, na.rm = TRUE),
-#                        Ongoing_Exchange = rowSums(ongoing_exchange, na.rm = TRUE),
-#                        Rnet = outdf$swRFV,
-#                        LE = outdf$latent,
-#                        CRV_BGC = outdf$crv)
-# plotdata[is.na(plotdata)] <- 0
-# 
-# nolabels <- theme(axis.title = element_blank(), axis.text.y = element_blank(), axis.ticks.y = element_blank())
-# baseplot <- ggplot(data = plotdata) + theme_minimal() + coord_flip() + nolabels
-# 
-# ylabels <- baseplot + geom_text(data
-# storage.plot <- baseplot + 
-#     geom_bar(aes(x = Biome, y = Storage), fill = "DarkGreen", width = 0.25, stat = "identity")
-# ongoing.plot <- baseplot +
-#     geom_bar(aes(x = Biome, y = Ongoing_Exchange), fill = "DarkBlue", width = 0.25, stat = "identity")
-# crv.plot <- baseplot +
-#     geom_bar(aes(x = Biome, y = CRV_BGC))
-# 
-# newplot <- grid.arrange(storage.plot, ongoing.plot, crv.plot, ncol = 4) 
-# 
+initial_storage <- outdf[,grepl("S_", colnames(outdf))]
+ongoing_exchange <- outdf[,grepl("F_", colnames(outdf))]
+Total_GHGV <- outdf[,grepl("GHGV_", colnames(outdf))]
+Biophysical <- data.frame(Rnet = outdf$swRFV, LE = outdf$latent)
+CRV <- outdf$crv
+
+## T_E is number of years considered
+T_E <- config.list$options$T_E
+xlabels <- paste0("CO_2 Emission Equivalents (Mg CO_2-eq ha-1 ",  T_E, " yrs-1)")
+
+library(ggplot2)
+library(gridExtra)
+
+library(Hmisc)
+
+plotdata <- data.frame(Biome = capitalize(paste(gsub("_", " ", gsub("BR", "Brazil", outdf$Biome)), "Site", outdf$Location)),
+                       Storage = rowSums(initial_storage, na.rm = TRUE),
+                       Ongoing_Exchange = rowSums(ongoing_exchange, na.rm = TRUE),
+                       Rnet = outdf$swRFV,
+                       LE = outdf$latent,
+                       CRV_BGC = outdf$crv)
+plotdata[is.na(plotdata)] <- 0
+
+nolabels <- theme(axis.title = element_blank(), axis.text.y = element_blank(), axis.ticks.y = element_blank())
+baseplot <- ggplot(data = plotdata) + theme_minimal() + coord_flip() + nolabels
+
+ylabels <- baseplot + geom_text(aes(x = plotdata$Biome, y = 1, label = gsub(" Site", "\nSite", plotdata$Biome)), 
+                                hjust = 1) + ylim(0,1) +
+  theme(axis.text.x = element_blank(), axis.ticks.x = element_blank()) 
+
+storage.plot <- baseplot + 
+  geom_bar(data = plotdata, aes(x = Biome, y = Storage), fill = "DarkGreen", width = 0.25, stat = "identity")
+ongoing.plot <- baseplot +
+  geom_bar(data = plotdata, aes(x = Biome, y = Ongoing_Exchange), fill = "DarkBlue", width = 0.25, stat = "identity")
+crv.plot <- baseplot +
+  geom_bar(data = plotdata, aes(x = Biome, y = CRV_BGC), fill = "Grey", width = 0.25, stat = "identity")
+
+grid.arrange(arrangeGrob(ylabels, storage.plot, ongoing.plot, crv.plot, ncol = 4,
+                         #main = "",
+                         #sub = textGrob(xlabels, hjust = 0.3)
+                         #sub = annotate("text", x = 2, y = 0.3, parse = T, label = xlabels)))
+
 
 
 
