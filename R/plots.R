674909afec19b4d4ab48b9a67d19056224e66797
ValentineHerr <herrmannv@si.edu>
2017-02-06 10:30:13 -0500

    Update plots.R
    Here are changes that:
    1- add black dots in the Biogeochemical and Biophysical plots
    2- make sure the order of the sites is correct: Even though the "Order" column was created, the bar plot in the ghgvc_subplot was messing up the order both because it would order Biomes by alphabetical order and because positive and negative values were handled separately.  I reordered Biome by the Order column and handled positive and negative values at the same time (using absolute or -absolute in the Biophysical plot). I hope this latter fix is okay.

--- R/plots.R
+++ R/plots.R
@@ -18,7 +18,7 @@ plot_ghgv <- function(df, output_dir,
                       years = 50,
                       save = TRUE, 
                       savefile = "output.svg") {
-
+  
   if (missing(output_dir) && save == TRUE) warning("output_dir must be specified if save is TRUE.")
   
   #Format data for plotting
@@ -27,16 +27,20 @@ plot_ghgv <- function(df, output_dir,
                              "Site",
                              df$Location)),
     Location = df$Location,
-    Order = vegtype_order(df$Biome),
+    Order = order(df$Location, vegtype_order(df$Biome)),
     Storage = rowSums(df[,grepl("S_", colnames(df))], na.rm = TRUE),
     Ongoing_Exchange = rowSums(df[,grepl("F_", colnames(df))], na.rm = TRUE),
     Rnet = df$swRFV,
     LE = df$latent)
-
+  
   #Create sums
   plotdata$CRV_BGC <- plotdata$Storage + plotdata$Ongoing_Exchange
+  plotdata$BGC_NET <- plotdata$Storage + plotdata$Ongoing_Exchange
+  
   plotdata$CRV_BIOPHYS <- plotdata$Rnet + plotdata$LE
-  plotdata$CRV_NET <- plotdata$CRV_BGC + plotdata$CRV_BIOPHYS
+  plotdata$BIOPHYS_NET <- -abs(plotdata$Rnet) + abs(plotdata$LE)
+  
+  plotdata$CRV_NET <- plotdata$BGC_NET + plotdata$BIOPHYS_NET
   
   #Replace NA/0 with 0
   plotdata[is.na(plotdata)] <- 0
@@ -48,7 +52,7 @@ plot_ghgv <- function(df, output_dir,
   #Don't plot CRV_BGC if Ongoing_Exchange is 0
   plotdata$CRV_NET[plotdata$CRV_BIOPHYS == 0] <- NA
   plotdata$CRV_BGC[plotdata$Ongoing_Exchange == 0] <- NA
-
+  
   ## Build data for subplots
   longdata <- gather(plotdata, variable, value, -Biome, -Location, -Order)
   longdata <- longdata[order(longdata$Location, longdata$Order, decreasing = FALSE), ]
@@ -69,14 +73,14 @@ plot_ghgv <- function(df, output_dir,
   
   #BGC
   bgc_plot <- ghgvc_subplot(c("Ongoing_Exchange", "Storage"), 
-                           data = longdata, 
-                           baseplot = baseplot)
+                            data = longdata, 
+                            baseplot = baseplot)
   bgc_plot <- bgc_plot + 
-             scale_fill_manual(values= brewer_pal(pal = "Greens")(6)[c(4,6)], 
-                               labels = c("Ongoing Exchange", "Storage")) + 
-             labs(fill = "") + 
-             ggtitle("Biogeochemical") + 
-             theme(axis.text.y = element_text(size = 12, hjust = 1)) 
+    scale_fill_manual(values= brewer_pal(pal = "Greens")(6)[c(4,6)], 
+                      labels = c("Ongoing Exchange", "Storage")) + 
+    labs(fill = "") + 
+    ggtitle("Biogeochemical") + 
+    theme(axis.text.y = element_text(size = 12, hjust = 1)) 
   bgc_plot 
   
   #BIOPHYS
@@ -84,15 +88,15 @@ plot_ghgv <- function(df, output_dir,
                                 data = longdata,
                                 baseplot = baseplot) 
   biophys_plot <- biophys_plot + 
-                 scale_fill_manual(values = brewer_pal(pal = "Blues")(6)[c(4,6)], 
-                                   labels = c(expression("Latent Heat Flux", "Net Radiation"))) + 
-                 labs(fill = "") + 
-                 ggtitle("Biophysical")
+    scale_fill_manual(values = brewer_pal(pal = "Blues")(6)[c(4,6)], 
+                      labels = c(expression("Latent Heat Flux", "Net Radiation"))) + 
+    labs(fill = "") + 
+    ggtitle("Biophysical")
   
   #CRV
-  crv_plot <-  ghgvc_subplot(c("CRV_BGC", "CRV_BIOPHYS"), 
-                            data = longdata,
-                            baseplot = baseplot) 
+  crv_plot <-  ghgvc_subplot(c("BGC_NET", "BIOPHYS_NET"), 
+                             data = longdata,
+                             baseplot = baseplot) 
   crv_plot <- crv_plot +
     scale_fill_manual(values= c(brewer_pal(pal = "Greens")(6)[5], 
                                 brewer_pal(pal = "Blues")(6)[5]), 
@@ -100,23 +104,35 @@ plot_ghgv <- function(df, output_dir,
     labs(fill = "") +
     ggtitle("Climate Regulating Value")
   
+  
+  #Add the net Biogeochemical point
+  bgc_plot <- bgc_plot +
+    geom_point(data = subset(longdata, variable == "BGC_NET"), 
+               aes(x = Biome, y = value))
+  
+  #Add the net Biophysical point
+  biophys_plot <- biophys_plot +
+    geom_point(data = subset(longdata, variable == "BIOPHYS_NET"), 
+               aes(x = Biome, y = value))
+  
+  
   #Add the net CRV point only if there is biophysical
   crv_plot <- crv_plot +
     geom_point(data = subset(longdata, variable == "CRV_NET"), 
                aes(x = Biome, y = value))
-
+  
   #Create the x label
   xlabels <- as.expression(bquote(paste("CO"[2], " Emission Equivalents (Mg CO"[2],"-eq ha"^{-1}, " ", .(years)," yrs"^{-1},")")))
   
   final_plot <- arrangeGrob(bgc_plot, 
-                             biophys_plot, 
-                             crv_plot, 
-                             ncol = 3, 
-                             widths = c(2,1,1),
-                             sub = textGrob(xlabels, 
-                                            x = unit(1, "npc"), 
-                                            y = unit(.9, "npc"),
-                                            just = c("centre", "bottom")))
+                            biophys_plot, 
+                            crv_plot, 
+                            ncol = 3, 
+                            widths = c(2,1,1),
+                            sub = textGrob(xlabels, 
+                                           x = unit(1, "npc"), 
+                                           y = unit(.9, "npc"),
+                                           just = c("centre", "bottom")))
   if(save) {
     #Create the SVG Plot image
     svg(filename=file.path(output_dir, savefile), width = 10, height = 2.5 + 1.5*nrow(plotdata))
@@ -126,7 +142,7 @@ plot_ghgv <- function(df, output_dir,
     #                            ncol = 3, 
     #                            widths = c(2,1,1),
     #                            textGrob(xlabels, just = c("centre", "bottom")))
-      grid.arrange(final_plot)
+    grid.arrange(final_plot)
     dev.off()
     
     #Fix to remove overflow
@@ -137,6 +153,7 @@ plot_ghgv <- function(df, output_dir,
   }
   final_plot
 }
+
 
 #' Plot ghgcv subplot.
 #' 
@@ -155,36 +172,42 @@ ghgvc_subplot <- function(vars, data, baseplot) {
   print(head(d))
   d$variable <- factor(d$variable, levels = vars)
 
+  d$Biome <- reorder(d$Biome, rev(d$Order))
+  
+  if(all(vars == c("LE", "Rnet"))){
+    d[d$variable == "LE",]$value <- abs(d[d$variable == "LE", ]$value )
+    d[d$variable == "Rnet",]$value <- -abs(d[d$variable == "Rnet", ]$value )
+  }
+
   # plot <- geom_bar(data = d,
   #                  aes(x = Biome, y = value, fill = variable),
   #                  width = 0.25, stat = "identity")
   # return(baseplot + plot)
   
   # #Positive Plot
-  pos <- d$value > 0
-  pos[is.na(pos)] <- FALSE #fix cases with NA
+  # pos <- d$value > 0
+  # pos[is.na(pos)] <- FALSE #fix cases with NA
   
-  posplot <- if(any(pos)) {
-    geom_bar(data = d[pos,],
+  # posplot <- if(any(pos)) {
+  posplot <-geom_bar(data = d, #[pos,],
              aes(x = Biome, y = value, fill = variable),
              width = 0.25, stat = "identity")
-  } else {
-    NULL
-  }
-
-  #Negative Plot
-  negplot <- if(any(!pos)) {
-    geom_bar(data = d[!pos,],
-             aes(x = Biome, y = value, fill = variable),
-             width = 0.25, stat = "identity")
-  } else {
-    NULL
-  }
-
+  # } else {
+  #   NULL
+  # }
+  
+  # #Negative Plot
+  # negplot <- if(any(!pos)) {
+  #   geom_bar(data = d[!pos,],
+  #            aes(x = Biome, y = value, fill = variable),
+  #            width = 0.25, stat = "identity")
+  # } else {
+  #   NULL
+  # }
+  
   #return all plots as one object
-  return(baseplot + negplot + posplot)
+  return(baseplot + posplot) # + negplot
 }
-
 
 
 
